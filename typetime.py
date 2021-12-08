f7c709e8732e1a493616eab419dba9407d51e419
Samuel Tan <xinyizhentan@gmail.com>
2021-12-08 01:15:22 -0500

    rudimentary timing proof of concept

--- typetime.py
+++ typetime.py
@@ -1,12 +1,19 @@
 from pynput import keyboard
+import time
 
 def on_press(key):
+    global last_time
+    new_time = time.perf_counter()
     try:
         print('alphanumeric key {0} pressed'.format(
             key.char))
     except AttributeError:
         print('special key {0} pressed'.format(
             key))
+    ms = (new_time - last_time)*1000
+    wpm = 12000/ms
+    print("Time between keystrokes was {0} ms ({1} wpm)".format(ms, wpm))
+    last_time = new_time
 
 def on_release(key):
     print('{0} released'.format(
@@ -15,6 +22,11 @@ def on_release(key):
         # Stop listener
         return False
 
+
+print("Press 'esc' to exit")
+
+last_time = time.perf_counter()
+
 # Collect events until released
 with keyboard.Listener(
         on_press=on_press,
@@ -22,8 +34,8 @@ with keyboard.Listener(
     listener.join()
 
 # ...or, in a non-blocking fashion:
-listener = keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release)
-listener.start()
+# listener = keyboard.Listener(
+#     on_press=on_press,
+#     on_release=on_release)
+# listener.start()
 
