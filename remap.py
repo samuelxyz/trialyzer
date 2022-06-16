c8e1c9cae5b966b0c9a364d30c3e1f5788f30048
Samuel Tan <xinyizhentan@gmail.com>
2022-06-16 16:20:09 -0400

    remap math

--- remap.py
+++ remap.py
@@ -1,4 +1,7 @@
 # Object to represent a superset of swaps, cycles, row/column swaps, etc
+# Remaps can be composed with each other using the + operator,
+# and reversed using the - operator. Subtraction is also implemented but
+# I have no clue when you would ever use that.
 
 from typing import Container, Iterable, Sequence
 
@@ -50,6 +53,28 @@ def col_swap(layout_: Layout, c1: int, c2: int,
             remap[otherkey] = key
     return remap
 
+def layout_diff(initial: Layout, target: Layout):
+    """Ignores fingermaps. Skips keys that are present on one layout 
+    but not the other.
+    """
+    remap = Remap()
+    for ipos, src in initial.keys.items():
+        try:
+            if src in remap:
+                continue
+            tpos = target.positions[src]
+            if ipos == tpos:
+                continue
+            dest = initial.keys[tpos]
+            first = src
+            while dest != first:
+                remap[src] = dest
+                src = dest
+                dest = initial.keys[target.positions[src]]
+        except KeyError:
+            continue
+    return remap
+
 class Remap(dict):
     
     def translate(self, ngram: Iterable[str]) -> tuple[str, ...]:
@@ -78,15 +103,50 @@ class Remap(dict):
                 cycles.append(sequence)
             else:
                 swaps.append(sequence)
-        src, dest = zip(*swaps)
-        descriptions.append(f"{' '.join(src)} <-> {' '.join(dest)}")
+        if swaps:
+            src, dest = zip(*swaps)
+            descriptions.append(f"{' '.join(src)} <-> {' '.join(dest)}")
         for cycle_ in cycles:
             descriptions.append(f"{' '.join(cycle_)} cycle")
         
         return ", ".join(descriptions)
 
-if __name__ == "__main__":
+    def __add__(self, other: type["Remap"]):
+        if not isinstance(other, Remap):
+            return NotImplemented
+        result = Remap()
+        for src, dest in self.items():
+            result[src] = other.get(dest, dest)
+        for src, dest in other.items():
+            if src not in result:
+                result[src] = dest
+        return Remap((k, v) for k, v in result.items() if k != v)
+
+    def __neg__(self):
+        result = Remap()
+        for src, dest in self.items():
+            result[dest] = src
+        return result
+
+    def __sub__(self, other):
+        return self + (-other)
+
+if __name__ == "__main__": # for testing
     import layout
+
     qwerty = layout.get_layout('qwerty')
-    r = col_swap(qwerty, 2, 3)
-    print(r)
+    cmk = layout.get_layout('colemak')
+    dh = layout.get_layout('colemak_dh')
+    dh_ansi = layout.get_layout('colemak_ca_ansi')
+    
+    # print(layout_diff(cmk, dh))
+    # print(repr(cmk))
+    # print(repr(dh))
+
+    dh_remap = layout_diff(cmk, dh)
+    print(dh_remap - dh_remap)
+
+    print(layout_diff(qwerty, cmk))
+    print(repr(qwerty))
+    print(repr(cmk))
+
